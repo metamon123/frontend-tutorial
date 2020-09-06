65f9ce55b126ad80e606bb6f03034e540b2d1e1e
Dongpyeong Seo <bludrimjr@gmail.com>
2020-09-07 01:42:50 +0900

    vue2: 기본적인 TodoList

---
+++ vue2/src/components/TodoList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <input type="text" v-model="input" placeholder="Add TodoItem" />
+    <input type="submit" @click="addTodoItem()" />
+    <br>
+    <span>Total {{ count }} items</span>
+    <br>
+    <ul>
+      <li v-for="(item, index) in todoList" :key="item">
+        {{ item }}
+        <button @click="deleteTodoItem(index)">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    data() {
+        return {
+            input: "",
+            todoList: [],
+        }
+    },
+    methods: {
+        addTodoItem() {
+            if (this.input) {
+                this.todoList.push(this.input);
+                this.input = "";
+            }
+        },
+
+        deleteTodoItem(index: number) {
+            this.todoList.splice(index, 1);
+        }
+    },
+    computed: {
+        count(): number {
+            return this.todoList.length;
+        }
+    }
+});
+</script>
---
+++ vue2/src/views/Todo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h2>Your Todo List</h2>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <TodoList />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import TodoList from "@/components/TodoList.vue";
+
+export default Vue.extend({
+    components: {
+        TodoList
+    }
+});
+</script>
